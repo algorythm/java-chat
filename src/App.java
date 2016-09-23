55990c6ff51454536096b69f91a72521942ae1dc
Anders W. Olsen <anders@wiberg.tech>
2016-09-24 00:33:49 +0200

    Chat working

--- src/App.java
+++ src/App.java
@@ -3,6 +3,29 @@
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hej");
+        if (args.length != 1 || args.length != 2)
+        {
+            System.out.println("Usage: ");
+            System.out.println("   Server: $ java -jar chat.jar <port>");
+            System.out.println("   Client: $ java -jar chat.jar <ip-address|hostname> <port>");
+        }
+        if (args.length == 1)
+        {
+            Server server = null;
+
+            System.out.println("==== SERVER ====");
+            System.out.println();
+
+            server = new Server(Integer.parseInt(args[0]));
+        }
+        if (args.length == 2)
+        {
+            Client client = null;
+
+            System.out.println("==== CLIENT ====");
+            System.out.println();
+
+            client = new Client(args[0], Integer.parseInt(args[1]));
+        }
     }
 }
